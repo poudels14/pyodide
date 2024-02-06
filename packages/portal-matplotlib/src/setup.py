66130702c48811ac29e95201f2437ce009d045f1
Sagar <poudels14@gmail.com>
2024-02-05 16:41:44 -0800

    add portal packages

---
+++ packages/portal-matplotlib/src/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="portal-matplotlib",
+    version="0.0.1",
+    author="Sagar Poudel",
+    packages=["portal-matplotlib"],
+)
---
+++ packages/portal/src/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="portal",
+    version="0.0.1",
+    author="Sagar Poudel",
+    packages=["portal"],
+)
